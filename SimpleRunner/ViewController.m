ec670ad13f7c974727e9c17e85242b5b99419d00
寺川 栄二 <eiji.terakawa@gmail.com>
2014-10-12 11:14:38 +0900

    距離計測テスト版

--- SimpleRunner/ViewController.m
+++ SimpleRunner/ViewController.m
@@ -31,7 +31,9 @@
 @property NSDate *startTime;
 @property NSDate *kmTime;
 @property NSDate *mTime;
-
+@property NSTimer *tm;
+@property CLLocation *previusLocaton;
+@property CLLocation *nextLocaton;
 
 @end
 
@@ -62,15 +64,8 @@
         [self.locationManager startUpdatingLocation];
 
         // タイマーの生成例
-        NSTimer *tm =
-        [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(timeUp:) userInfo:nil repeats:YES
+        self.tm = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(timeUp:) userInfo:nil repeats:YES
          ];
-        if(![tm isValid]){
-            [tm fire];
-        }
-        if([tm isValid]){
-            [tm invalidate];
-        }
     }
 }
 
@@ -78,16 +73,68 @@
 {
     NSLog(@"viewDidAppear");
     [super viewDidAppear:animated];
+    if(![self.tm isValid]){
+        [self.tm fire];
+    }
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
     NSLog(@"viewDidDisappear");
     [super viewDidDisappear:animated];
+    if([self.tm isValid]){
+        [self.tm invalidate];
+    }
 }
 
 - (void)timeUp:(NSTimer*)timer{
     NSLog(@"TimeUp");
+
+    if(self.previusLocaton!=nil){
+        if(self.nextLocaton!=nil){
+            //　距離を取得
+            CLLocationDistance distance = [self.previusLocaton distanceFromLocation:self.nextLocaton];
+            self.totalDistance = self.totalDistance + distance;
+            self.kmDistance = self.kmDistance + distance;
+            self.mDistance = self.mDistance + distance;
+            
+            self.previusLocaton = self.nextLocaton;
+            
+            if(self.kmDistance > 1000){
+                NSDate *now = [NSDate date];
+                float tmp= [now timeIntervalSinceDate:self.kmTime];
+                // 時
+                int hour = (int)(tmp / 3600);
+                // 分
+                int min = (int)(tmp / 60);
+                // 秒
+                int sec = (int)tmp % 60;
+                
+                self.kmLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分  %zd秒",hour,min,sec];
+            }
+            if(self.mDistance > 100){
+                NSDate *now = [NSDate date];
+                float tmp= [now timeIntervalSinceDate:self.mTime];
+                // 時
+                int hour = (int)(tmp / 3600);
+                // 分
+                int min = (int)(tmp / 60);
+                // 秒
+                int sec = (int)tmp % 60;
+                
+                self.mLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分  %zd秒",hour,min,sec];
+            }
+            
+            // 距離をコンソールに表示
+            NSLog(@"distance:%f", distance);
+            NSLog(@"total distance:%f", self.totalDistance);
+            if(self.totalDistance < 1000){
+                self.distanceLabel.text=[NSString stringWithFormat:@"%zdm",self.totalDistance];
+            }else{
+                self.distanceLabel.text=[NSString stringWithFormat:@"%.2fkm",self.totalDistance/1000];
+            }
+        }
+    }
 }
 
 - (IBAction)startUP:(UIButton *)sender {
@@ -101,13 +148,13 @@
 }
 
 - (IBAction)start:(UIButton *)sender {
-    if(self.statusStart == FALSE){
-        self.startLabel.titleLabel.text = @"END";
-        self.statusStart = TRUE;
-    }else{
-        self.startLabel.titleLabel.text = @"START";
-        self.statusStart = FALSE;
-    }
+//    if(self.statusStart == FALSE){
+//        self.startLabel.titleLabel.text = @"END";
+//        self.statusStart = TRUE;
+//    }else{
+//        self.startLabel.titleLabel.text = @"START";
+//        self.statusStart = FALSE;
+//    }
 }
 
 // 位置情報が更新されるたびに呼ばれる
@@ -116,66 +163,39 @@
            fromLocation:(CLLocation *)oldLocation
 {
     NSString *m;
-    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.latitude];
-    NSLog(@"Latitude = %@",m);
-    
-    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.longitude];
-    NSLog(@"Longitude = %@",m);
+//    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.latitude];
+//    NSLog(@"Latitude = %@",m);
+//    
+//    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.longitude];
+//    NSLog(@"Longitude = %@",m);
 
-    self.locationLabel.text=[NSString stringWithFormat:@"%.2f:%.2f",newLocation.coordinate.latitude,newLocation.coordinate.longitude];
+    self.locationLabel.text=[NSString stringWithFormat:@"緯度:%.2f  経度:%.2f",newLocation.coordinate.latitude,newLocation.coordinate.longitude];
     
-    //　距離を取得
-    CLLocationDistance distance = [newLocation distanceFromLocation:oldLocation];
-    self.totalDistance = self.totalDistance + distance;
-    self.kmDistance = self.kmDistance + distance;
-    self.mDistance = self.mDistance + distance;
-
-    if(self.kmDistance > 1000){
-        NSDate *now = [NSDate date];
-        float tmp= [now timeIntervalSinceDate:self.kmTime];
-        // 時
-        int hour = (int)(tmp / 3600);
-        // 分
-        int min = (int)(tmp / 60);
-        // 秒
-        int sec = (int)tmp % 60;
-        
-        self.kmLabel.text = [NSString stringWithFormat:@"%zdH %zdM  %zdS",hour,min,sec];
+    // 位置情報を設定
+    if(self.previusLocaton == nil){
+        NSLog(@"初回設定");
+        self.previusLocaton = newLocation;
     }
-    if(self.mDistance > 100){
-        NSDate *now = [NSDate date];
-        float tmp= [now timeIntervalSinceDate:self.mTime];
-        // 時
-        int hour = (int)(tmp / 3600);
-        // 分
-        int min = (int)(tmp / 60);
-        // 秒
-        int sec = (int)tmp % 60;
-        
-        self.mLabel.text = [NSString stringWithFormat:@"%zdH %zdM  %zdS",hour,min,sec];
-    }
+    NSLog(@"現在設定");
+    self.nextLocaton = newLocation;
     
-    // 距離をコンソールに表示
-    NSLog(@"distance:%f", distance);
-    NSLog(@"total distance:%f", self.totalDistance);
-    self.distanceLabel.text=[NSString stringWithFormat:@"%f",self.totalDistance];
-    
+    // 住所を取得
     CLGeocoder *geocoder = [CLGeocoder new];
     CLLocation *location = [[CLLocation new] initWithLatitude:newLocation.coordinate.latitude
                                                     longitude:newLocation.coordinate.longitude];
-    NSLog(@"location = %@",location);
+//    NSLog(@"location = %@",location);
     [geocoder reverseGeocodeLocation:location
                    completionHandler:^(NSArray* placemarks, NSError* error) {
                        // 経度、緯度から逆ジオコーディングを行った結果（場所）の数
-                       NSLog(@"逆ジオ変換完了");
+//                       NSLog(@"逆ジオ変換完了");
                        if(error){
-                           NSLog(@"Error!");
+//                           NSLog(@"Error!");
                        }else{
-                           NSLog(@"found : %zd", [placemarks count]);
+//                           NSLog(@"found : %zd", [placemarks count]);
                            
                            for (CLPlacemark *placemark in placemarks) {
                                // それぞれの結果（場所）の情報
-                               NSLog(@"%@%@",placemark.locality,placemark.name);
+//                               NSLog(@"%@%@",placemark.locality,placemark.name);
                                self.addressLabel.text=[NSString stringWithFormat:@"%@%@",placemark.locality,placemark.name];
 
                                
