a91509b8bfe91524e8abd6cabdec580034f6f008
寺川 栄二 <eiji.terakawa@gmail.com>
2014-10-12 21:59:07 +0900

    音声を追加

--- SimpleRunner/ViewController.m
+++ SimpleRunner/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import <CoreLocation/CoreLocation.h>
 #import <CoreLocation/CLGeocoder.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController () <CLLocationManagerDelegate>
 
@@ -33,6 +34,7 @@
 @property NSTimer *tm;
 @property CLLocation *previusLocaton;
 @property CLLocation *nextLocaton;
+@property NSString *totalDistanceString;
 
 @end
 
@@ -48,23 +50,10 @@
         self.locationManager = [CLLocationManager new];
         self.locationManager.delegate = self;
 
-//        self.totalDistance = 0;
-//        self.kmDistance = 0;
-//        self.mDistance = 0;
-//        self.statusStart = FALSE;
-//        self.startLabel.titleLabel.text = @"START";
-//        
-//	    self.startTime = [NSDate date];
-//        self.kmTime = [NSDate date];
-//        self.mTime = [NSDate date];
-//        
         // 位置情報の取得開始
         NSLog(@"位置情報取得開始");
         [self.locationManager startUpdatingLocation];
 
-//        // タイマーの生成
-//        self.tm = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(timeUp:) userInfo:nil repeats:YES
-//         ];
     }
 }
 
@@ -91,23 +80,33 @@
 - (void)timeUp:(NSTimer*)timer{
     NSLog(@"TimeUp");
 
+    // AVSpeechSynthesizerを初期化する。
+    AVSpeechSynthesizer* speechSynthesizer = [AVSpeechSynthesizer new];
+    
     NSDate *now = [NSDate date];
     float tmp= [now timeIntervalSinceDate:self.startTime];
     // 時
     int hour = (int)(tmp / 3600);
     // 分
-    int min = (int)(tmp / 60);
+    int min = (int)tmp % 3600 / 60;
     // 秒
     int sec = (int)tmp % 60;
     
-    self.timeLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分  %zd秒",hour,min,sec];
-
+    if(hour != 0){
+        self.timeLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分 %zd秒",hour,min,sec];
+    }else if(min != 0){
+        self.timeLabel.text = [NSString stringWithFormat:@"%zd分 %zd秒",min,sec];
+    }else{
+        self.timeLabel.text = [NSString stringWithFormat:@"%zd秒",sec];
+    }
+    
     
     if(self.previusLocaton!=nil){
         if(self.nextLocaton!=nil){
             //　距離を取得
-            CLLocationDistance distance = [self.previusLocaton distanceFromLocation:self.nextLocaton];
-//            double distance = 20;
+//            CLLocationDistance distance = [self.previusLocaton distanceFromLocation:self.nextLocaton];
+            double distance = 25;
+
             self.totalDistance = self.totalDistance + distance;
             self.kmDistance = self.kmDistance + distance;
             self.mDistance = self.mDistance + distance;
@@ -124,8 +123,25 @@
                 int min = (int)tmp % 3600 / 60;
                 // 秒
                 int sec = (int)tmp % 60;
+                if(hour != 0){
+                    self.kmLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分 %zd秒",hour,min,sec];
+                }else if(min != 0){
+                    self.kmLabel.text = [NSString stringWithFormat:@"%zd分 %zd秒",min,sec];
+                }else{
+                    self.kmLabel.text = [NSString stringWithFormat:@"%zd秒",sec];
+                }
                 
-                self.kmLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分  %zd秒",hour,min,sec];
+                // AVSpeechUtteranceを読ませたい文字列で初期化する。
+                NSString* speakingText = [NSString stringWithFormat:@"1キロメートルの　タイムは　%@ です",self.kmLabel.text];
+                AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+                utterance.rate = 0.3f;        //読み上げる速さ
+                
+                // AVSpeechSynthesizerにAVSpeechUtteranceを設定して読んでもらう
+                [speechSynthesizer speakUtterance:utterance];
+                
+                
+                
+
             }
             if(self.mDistance > 100){
                 float tmp= [now timeIntervalSinceDate:self.mTime];
@@ -138,23 +154,53 @@
                 // 秒
                 int sec = (int)tmp % 60;
                 
-                self.mLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分  %zd秒",hour,min,sec];
+                if(hour != 0){
+                    self.mLabel.text = [NSString stringWithFormat:@"%zd時間 %zd分 %zd秒",hour,min,sec];
+                }else if(min != 0){
+                    self.mLabel.text = [NSString stringWithFormat:@"%zd分 %zd秒",min,sec];
+                }else{
+                    self.mLabel.text = [NSString stringWithFormat:@"%zd秒",sec];
+                }
+
+                // AVSpeechUtteranceを読ませたい文字列で初期化する。
+                NSString* speakingText = [NSString stringWithFormat:@"100メートルの　タイムは　%@ です",self.mLabel.text];
+                AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+                utterance.rate = 0.3f;        //読み上げる速さ
+                
+                // AVSpeechSynthesizerにAVSpeechUtteranceを設定して読んでもらう
+                [speechSynthesizer speakUtterance:utterance];
             }
-            
+
             // 距離をコンソールに表示
-            NSLog(@"distance:%f", distance);
             NSLog(@"total distance:%f", self.totalDistance);
             if(self.totalDistance < 1000){
                 self.distanceLabel.text=[NSString stringWithFormat:@"%zdm",(int)self.totalDistance];
+                self.totalDistanceString = [NSString stringWithFormat:@"%zd　メートルの",(int)self.totalDistance];
             }else{
                 self.distanceLabel.text=[NSString stringWithFormat:@"%.2fkm",self.totalDistance/1000];
+                self.totalDistanceString = [NSString stringWithFormat:@"%.2f　キロメートルの",self.totalDistance/1000];
             }
+            
+            if(sec == 0){
+                // AVSpeechUtteranceを読ませたい文字列で初期化する。
+                NSString* speakingText = [NSString stringWithFormat:@"きょり  %@  タイムは　%@ です",self.totalDistanceString,self.timeLabel.text];
+                AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+                utterance.rate = 0.3f;        //読み上げる速さ
+                
+                // AVSpeechSynthesizerにAVSpeechUtteranceを設定して読んでもらう
+                [speechSynthesizer speakUtterance:utterance];
+            }
+            
         }
     }
 }
 
 - (IBAction)start:(UIButton *)sender {
     NSLog(@"toutch");
+    
+    // AVSpeechSynthesizerを初期化する。
+    AVSpeechSynthesizer* speechSynthesizer = [AVSpeechSynthesizer new];
+
     if(self.statusStart == FALSE){
         NSLog(@"END");
         [self.startLabel setTitle:@"END" forState:UIControlStateNormal];
@@ -176,6 +222,13 @@
             [self.tm fire];
         }
     
+        // AVSpeechUtteranceを読ませたい文字列で初期化する。
+        NSString* speakingText = @"計測を開始します。";
+        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+        utterance.rate = 0.3f;        //読み上げる速さ
+        
+        // AVSpeechSynthesizerにAVSpeechUtteranceを設定して読んでもらう
+        [speechSynthesizer speakUtterance:utterance];
     
     
     }else{
@@ -185,6 +238,16 @@
         if([self.tm isValid]){
             [self.tm invalidate];
         }
+
+        // AVSpeechUtteranceを読ませたい文字列で初期化する。
+        NSString* speakingText = @"計測を終了しました。";
+        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+        utterance.rate = 0.3f;        //読み上げる速さ
+        
+        // AVSpeechSynthesizerにAVSpeechUtteranceを設定して読んでもらう
+        [speechSynthesizer speakUtterance:utterance];
+
+    
     }
 }
 
@@ -194,11 +257,11 @@
            fromLocation:(CLLocation *)oldLocation
 {
     NSString *m;
-//    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.latitude];
-//    NSLog(@"Latitude = %@",m);
-//    
-//    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.longitude];
-//    NSLog(@"Longitude = %@",m);
+    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.latitude];
+    NSLog(@"Latitude = %@",m);
+    
+    m = [NSString stringWithFormat:@"%.2f", newLocation.coordinate.longitude];
+    NSLog(@"Longitude = %@",m);
 
     self.locationLabel.text=[NSString stringWithFormat:@"緯度:%.2f  経度:%.2f",newLocation.coordinate.latitude,newLocation.coordinate.longitude];
     
@@ -210,24 +273,23 @@
     NSLog(@"現在設定");
     self.nextLocaton = newLocation;
     
-//    return;
     // 住所を取得
     CLGeocoder *geocoder = [CLGeocoder new];
     CLLocation *location = [[CLLocation new] initWithLatitude:newLocation.coordinate.latitude
                                                     longitude:newLocation.coordinate.longitude];
-//    NSLog(@"location = %@",location);
+    NSLog(@"location = %@",location);
     [geocoder reverseGeocodeLocation:location
                    completionHandler:^(NSArray* placemarks, NSError* error) {
                        // 経度、緯度から逆ジオコーディングを行った結果（場所）の数
-//                       NSLog(@"逆ジオ変換完了");
+                       NSLog(@"逆ジオ変換完了");
                        if(error){
-//                           NSLog(@"Error!");
+                           NSLog(@"Error!");
                        }else{
-//                           NSLog(@"found : %zd", [placemarks count]);
+                           NSLog(@"found : %zd", [placemarks count]);
                            
                            for (CLPlacemark *placemark in placemarks) {
                                // それぞれの結果（場所）の情報
-//                               NSLog(@"%@%@",placemark.locality,placemark.name);
+                               NSLog(@"%@%@",placemark.locality,placemark.name);
                                self.addressLabel.text=[NSString stringWithFormat:@"%@%@",placemark.locality,placemark.name];
 
                                
